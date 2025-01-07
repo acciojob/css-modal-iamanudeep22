2a2067782fe3e5faa20704047536daf87f328da6
Anudeep KN <116871481+iamanudeep22@users.noreply.github.com>
2025-01-07 14:28:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// JavaScript to open/close the modal
+document.getElementById('openModal').onclick = function() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+// Close the modal when clicking outside of it or on the close button
+window.onclick = function(event) {
+  var modal = document.getElementById('myModal');
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+document.querySelector('.close-modal').onclick = function() {
+  document.getElementById('myModal').style.display = "none";
+}
